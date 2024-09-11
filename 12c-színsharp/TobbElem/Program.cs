d1cbec44a9ca3ccf74a8c6065e1fc547dea3c92f
Hunor Bereczki <2021122@dszcberegszaszi.hu>
2024-09-11 10:09:53 +0200

    TobbElem

--- 12c-színsharp/TobbElem/Program.cs
+++ 12c-színsharp/TobbElem/Program.cs
@@ -11,6 +11,30 @@ namespace TobbElem
         static void Main(string[] args)
         {
             Console.WriteLine("Első feladat");
+            int a = 0;
+            int paros = 0;
+            int paratlan = 0;
+            while (true)
+            {
+                Console.Write("Kérek egy számot: ");
+                int b = Convert.ToInt32(Console.ReadLine());
+                a += b;
+                if (b % 2 == 0)
+                {
+                    paros++;
+                }
+                else
+                {
+                    paratlan++;
+                }
+                if (b > 100)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Páros: " + paros + "Páratlan: " + paratlan);
+            Console.ReadLine();
+            
         }
     }
 }
