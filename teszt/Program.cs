f0449b3e3b31d1570250590e677bd10091b6a54f
Hunor Bereczki <2021122@dszcberegszaszi.hu>
2024-09-11 08:53:38 +0200

    1.

---
+++ teszt/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teszt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ide majd írok valamit
+            Console.WriteLine("Ide írtam valamit!");
+        }
+    }
+}
